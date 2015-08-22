6f060c15a48dce2864f8d2e1662dcf5c44ca29fd
Zeust the Unoobian <2noob2banoob@gmail.com>
2015-08-22 21:48:23 +0200

    Documented backend

--- technivaldb.php
+++ technivaldb.php
@@ -4,32 +4,56 @@ Class TechnivalDB {
 	private $tablename;
 	private $tablespec =
 		"id INTEGER PRIMARY KEY, name text NOT NULL, occasion integer NOT NULL";
-	private $con;
-	private $st_insert;
-	private $sql_read;
+	private $con;       /** Database connection */
+	private $st_insert; /** Compiled insertion SQL statement */
+	private $sql_read;  /** Non-compiled fetching SQL statement */
 	
+	/**
+	 * Constructor. Opens database connection and constructs SQL statement templates.
+	 * Also creates database and table if they do not exist.
+	 * Parameters:
+	 *   $db:       database name
+	 *   $table:    table name
+	 *   $recreate: (optional, default=false) if true, recreate table even if it already
+	 *              exists. This will delete all entries and update the table
+	 *              specification if it has changed in the code.
+	 */
 	public function __construct($db, $table, $recreate=false) {
 		// Open database connection
 		$this->dbname = $db;
 		$this->con = new PDO("sqlite:".$this->dbname);
 		if(!$this->con) throw new Exception("Could not open database: ".$error);
+		// Other initialisation stuff
 		$this->tablename = $this->con->quote($table);
 		$this->create_table($recreate);
 		$this->compile_statements();
 	}
 	
+	/**
+	 * Create the table if it does not exist or if $ifexists (optional, default=false)
+	 * is true. If the table is created, it will be empty and according to the
+	 * specification in the current version of the code.
+	 */
 	private function create_table($ifexists=false) {
 		if($ifexists) $this->con->exec("DROP TABLE IF EXISTS $this->tablename");
 		$sql = "CREATE TABLE IF NOT EXISTS $this->tablename($this->tablespec)";
 		if($this->con->exec($sql) === false) $this->report_error("Could not create table");
 	}
 	
+	/**
+	 * Finish SQL statement templates by inserting the table name in them,
+	 * and precompile them if possible.
+	 */
 	private function compile_statements() {
 		$this->sql_read = "SELECT name, occasion FROM $this->tablename";
 		$this->st_insert = $this->con->prepare("INSERT INTO $this->tablename VALUES(NULL, ?, ?)");
 		if(!$this->st_insert) $this->report_error("Cannot compile insert statement");
 	}
 	
+	/**
+	 * Raise an Exception with the message given in $msg. If a database
+	 * connection is open, append the latest database error before raising.
+	 */
 	private function report_error($msg) {
 		if($this->con) {
 			$e = $this->con->errorInfo();
@@ -38,15 +62,27 @@ Class TechnivalDB {
 		throw new Exception("<p>$msg</p>");
 	}
 	
+	/**
+	 * Insert a new entry into the database for person $name participating
+	 * during occasion $occasion (optional, default=1).
+	 */
 	public function insert($name, $occasion=1) {
 		if($this->st_insert->execute(array($name, $occasion)) === false)
 			$this->report_error("Cannot insert $name at occasion $occasion");
 	}
 	
+	/**
+	 * Fetch all participants. Returns an array with both numeric and
+	 * associative keys.
+	 */
 	public function get_stuff() {
 		return $this->con->query($this->sql_read) -> fetchAll();
 	}
 	
+	/**
+	 * Close database connection. This is irreversible and the
+	 * TechnivalDB object is useless after this.
+	 */
 	public function close(){
 		$this->con = null;
 	}
